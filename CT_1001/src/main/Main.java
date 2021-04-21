cac96c1f4a6fe066f8979b644f95e0986b8ed8dc
kjs63 <kjs63@192.168.123.105>
2021-04-22 01:23:56 +0900

    A-B

---
+++ CT_1001/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// A - B 구하기
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a-b);
+
+	}
+
+}
