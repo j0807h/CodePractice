b775a628b0b0e03cd13d0205f70e028a92ff0c58
최정화 <purity_c@gemvaxlink.com>
2021-10-19 23:58:47 +0900

    백준 입출력과 사칙연산 문제8

---
+++ CT_1008/src/main/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * <문제>
+ * 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+ * 
+ * <입력>
+ * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ * 
+ * <출력>
+ * 첫째 줄에 A/B를 출력한다.
+ * 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
+ * */
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] nArr = new int[2];
+		for(int i=0; i<2; i++) {
+			nArr[i] = Integer.parseInt(sc.next());
+		}
+		double A = nArr[0];
+		double B = nArr[1];
+		
+		double result = A/B;
+		
+		System.out.println(result);
+		
+	}
+
+}
